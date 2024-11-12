12f40fb3fd397ae4ebc004208c38c614b903157e
Nam <phanhainam20035@gmail.com>
2024-11-12 15:36:06 +0700

    Add .env file

--- config/connect.php
+++ config/connect.php
@@ -1,8 +1,8 @@
 <?php
-    $server = "localhost";
-    $user = "root";
-    $password = "";
-    $database = "crm_project";
+    $server = $_ENV['DB_SERVER'];
+    $user = $_ENV['DB_USERNAME'];
+    $password = $_ENV['DB_PASSWORD'];
+    $database = $_ENV['DB_DATABASE'];
     $conn = new mysqli($server, $user, $password, $database);
     if($conn){
         mysqli_query($conn, 'set names "utf8"');
