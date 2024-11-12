c4c5a087e82034c8d0c4e48c515be8fd313bfb73
Nam <phanhainam20035@gmail.com>
2024-11-12 16:20:25 +0700

    Update project

--- config/connect.php
+++ config/connect.php
@@ -1,4 +1,11 @@
 <?php
+    // Nạp autoloader của Composer
+    require_once __DIR__ . '/../vendor/autoload.php';
+
+    // Sử dụng vlucas/phpdotenv để nạp file .env
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+    $dotenv->load();
+
     $server = $_ENV['DB_SERVER'];
     $user = $_ENV['DB_USERNAME'];
     $password = $_ENV['DB_PASSWORD'];
